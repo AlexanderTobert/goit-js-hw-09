22ea762577887ca4f79d1196e619754e8807ecb4
AlexanderTobert <tobert.sasha@gmail.com>
2022-09-27 23:24:30 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ const bodyRef = document.querySelector('body');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+let timerId;
 btnStartRef.addEventListener('click', () => { 
     timerId = setInterval(() => {
         const bgnColor = getRandomHexColor()
