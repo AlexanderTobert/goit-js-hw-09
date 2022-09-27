bbb82af9b31e63d79728cbaf02bb3e4d2245a7ec
AlexanderTobert <99279731+AlexanderTobert@users.noreply.github.com>
2022-09-27 20:24:57 +0000

    Deploying to gh-pages from @ AlexanderTobert/goit-js-hw-09@22ea762577887ca4f79d1196e619754e8807ecb4 🚀

---
+++ 01-color-switcher.dec7b2b8.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.backgroundColor=t}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.dec7b2b8.js.map
